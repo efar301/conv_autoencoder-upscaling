faa3c8c2a2574f91c572e42d6ef1d8787e41425a
Evan Farnsworth <farnsworth.evan@yahoo.com>
2025-03-05 10:22:16 -0800

    changes

--- model_code/modelV2/fastUpscaler.py
+++ model_code/modelV2/fastUpscaler.py
@@ -1,14 +1,15 @@
 import torch.nn as nn
 import torch
+import torch.nn.init as init
 
 
 class resblock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(resblock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
+        # self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+        # self.bn2 = nn.BatchNorm2d(out_channels)
         self.silu = nn.SiLU()
         
         
@@ -21,9 +22,9 @@ class resblock(nn.Module):
         identity = x
         
         x = self.conv1(x)
-        x = self.bn1(x)
+        # x = self.bn1(x)
         x = self.conv2(x)
-        x = self.bn2(x)
+        # x = self.bn2(x)
         
         if self.downsample:
             identity = self.downsample(identity)
@@ -48,6 +49,7 @@ class fastUpscaler(nn.Module):
             resblock(channels, self.hidden_channels)
         )
         
+        
         self.heads = nn.ModuleDict()
         for factor in upscale_factors:
                 out_channels = 3 * (factor ** 2)
@@ -55,6 +57,9 @@ class fastUpscaler(nn.Module):
                     nn.Conv2d(self.hidden_channels, out_channels, kernel_size=3, stride=1, padding=1),
                     nn.PixelShuffle(factor)
                 )
+                
+                icnr_init(self.heads[str(factor)][0].weight, factor)
+        
         
     def forward(self, x, scale_factor):
         x = self.prelim(x)
@@ -64,6 +69,16 @@ class fastUpscaler(nn.Module):
         
         return x
     
+    
+
+def icnr_init(tensor, scale=2, init_method=init.kaiming_normal_):
+
+    subkernel_channels = tensor.shape[0] // (scale ** 2)
+    subkernel = torch.zeros((subkernel_channels, *tensor.shape[1:]), device=tensor.device)
+    init_method(subkernel)
+    subkernel = subkernel.repeat(scale ** 2, 1, 1, 1)
+    tensor.data.copy_(subkernel)
+   
 def test():
     x = torch.randn((3, 3, 640, 360))
     model = fastUpscaler(in_channels=3, channels=64, upscale_factors=[3, 4, 6])
